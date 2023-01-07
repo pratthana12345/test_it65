fe9f9d1371c9e6c148171170a0a2a0ab5fbec4ea
pratthana12345 <pratthanabam2544@gmail.com>
2023-01-07 12:59:44 +0700

    edit connect3

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
-    mysqli_set_charset($conn,'utf8');
+    mysqli_set_charset($conn,'UFT8');
     if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     }
